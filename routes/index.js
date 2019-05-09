11721145a0791b1edb4339a5b19daef74bb464ca
Martín S <martin@rindecuentas.org>
2019-05-09 18:55:28 -0500

    Adding RSS client

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  let feed = await getFeed();
+  res.render('index', { feed: feed });
 });
+
+async function getFeed() {
+  let Parser = require('rss-parser');
+  let parser = new Parser();
+
+  let feed = await parser.parseURL('https://www.rindecuentas.org/feed/');
+  return feed.items.slice(0,3);
+}
 
 module.exports = router;
