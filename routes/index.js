9b189436971562e297de20b4298451f4d06e4d61
Marisol Carrillo <marisol@rindecuentas.org>
2019-05-10 17:07:14 -0500

    Agregué slider y arreglé css

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
+  let feed = await getFeed();
   res.render('home', { feed: feed });
 });
 
