020a41f9068ee81522b202e3473907823652abb5
Martín S <martin@rindecuentas.org>
2019-05-14 17:22:58 -0500

    Wait for RSS request

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('home', { feed: feed });
+router.get('/', async function(req, res, next) {
+  res.render('home', { feed: await getFeed() });
 });
 
 async function getFeed() {
