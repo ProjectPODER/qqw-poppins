d5a5009ce1fcee230ffd0f05f275c738d04659ca
Marisol Carrillo <marisol@rindecuentas.org>
2019-08-09 17:00:00 -0500

    Updating archives

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -15,34 +15,6 @@ $(window).scroll(function() {
   }
 });*/
 
-// Footer carousel
-$('.owl-carousel').owlCarousel({
-  // center: true,
-  items:1,
-  loop:true,
-  margin:0,
-  dots:false,
-  nav:true,
-  navText:['<','>'],
-  autoplay:true,
-  autoplayTimeout:3000,
-  autoplayHoverPause:true,
-  responsive:{
-      1042:{
-          items:1
-      },
-      992:{
-          items:1
-      },
-      300:{
-          items:1
-      },
-      0:{
-          items:1
-      }
-  }
-});
-
 //Tooltips
 $('[data-toggle="tooltip"]').tooltip({placement: 'top'});
 
