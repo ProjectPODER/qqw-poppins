10e0b4fc01a13be00552cbc15daffc64436e0d34
Axel <axel.herrera@kronops.com.mx>
2019-10-02 11:02:58 -0500

    se arreglan los echos del script

--- docker_build.sh
+++ docker_build.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 source $HOME/allvars
-APP_PORT = 8086:8080
+APP_PORT=8086:8080
 export ENVIRONMENT="stg"
 
 build() {
-	echo -n "Building ${WEB_DOCKER_REPO} image."
+	echo -e "Building ${WEB_DOCKER_REPO} image."
 	docker build -t ${WEB_DOCKER_REPO} .
-	echo -n "Listing ${WEB_DOCKER_REPO} image."
+	echo -e "Listing ${WEB_DOCKER_REPO} image."
 	docker images
 }
 
 test() {
-	echo -n "Run ${WEB_DOCKER_REPO} image."
+	echo -e "Run ${WEB_DOCKER_REPO} image."
 	docker run --name ${WEB_APP_NAME} -p ${APP_PORT} -d ${WEB_DOCKER_REPO} &
-	echo -n "Wait until ${WEB_DOCKER_REPO} is fully started."
+	echo -e "Wait until ${WEB_DOCKER_REPO} is fully started."
 	sleep 10
 	docker logs ${WEB_APP_NAME}
 }
 
 release() {
-	echo -n "Release ${WEB_IMG} image."
+	echo -e "Release ${WEB_IMG} image."
 	if [[ ! -z "$DOCKER_PWD" ]]; then
 		cat ${DOCKER_PWD} | docker login --username ${DOCKER_USER} --password-stdin
 	fi
@@ -29,26 +29,25 @@ release() {
 }
 
 clean() {
-	echo -n ""
-	echo -n "Cleaning local build environment."
-	echo -n ""
+	echo -e ""
+	echo -e "Cleaning local build environment."
+	echo -e ""
 	docker stop ${WEB_APP_NAME} 2>/dev/null; true
 	docker rm ${WEB_APP_NAME}  2>/dev/null; true
-	echo -n ""
-	echo -n "Purging local images."
+	echo -e ""
+	echo -e "Purging local images."
 	docker rmi ${WEB_DOCKER_REPO} 2>/dev/null; true
 }
 
 help() {
-	echo -n ""
-	echo -n "Please use \`make <target>' where <target> is one of"
-	echo -n ""
-	echo -n "  build		Builds the docker image."
-	echo -n "  test		Tests image."
-	echo -n "  release	Releases images."
-	echo -n "  clean		Cleans local images."
-	echo -n ""
-	echo -n ""
+	echo -e ""
+	echo -e "Please use: 'bash $0 <target>' where <target> is one of"
+	echo -e ""
+	echo -e "  build		Builds the docker image."
+	echo -e "  test		Tests image."
+	echo -e "  release	Releases images."
+	echo -e "  clean		Cleans local images."
+	echo -e ""	
 }
 
 if [[ "$1" == "build" ]]; then build;
