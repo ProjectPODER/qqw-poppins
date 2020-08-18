4ea84719821b3b22744626e8f764fadb56d77d92
Martín S <martin@rindecuentas.org>
2020-08-18 18:36:06 -0500

    Redirect to language URLs (still no user choice) #119

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 var lib = require('../lib/lib.js');
 
+function createMultiLangRoutes(es,en,callback) {
+    router.get(es, lib.redirectToLanguage(es,en));
+    router.get('/es'+es, callback);
+    router.get('/en'+en, callback);
+}
+
 /* GET home page. */
-router.get('/', lib.homePage());
+createMultiLangRoutes('/', "/", lib.homePage());
 
 /* Redirect old search pages */
 router.get('/personas', lib.redirectToSearch("persons"));
@@ -14,61 +20,63 @@ router.get('/empresas', lib.redirectToSearch("companies"));
 router.get('/paises', lib.redirectToSearch("countries"));
 
 /* GET Searcher */
-router.get('/buscador', lib.searchPage2020());
+createMultiLangRoutes('/buscador', "/search", lib.searchPage2020());
 
 /* GET entity pages. */
-router.get('/personas/:id', lib.entityPage("persons","perfil","id"));
-router.get('/instituciones-publicas/:id', lib.entityPage("institutions","perfil","id"));
-router.get('/empresas/:id', lib.entityPage("companies","perfil", "id"));
-router.get('/regiones/:id', lib.entityPage("areas","perfil","id"));
+createMultiLangRoutes('/personas/:id', "/persons/:id", lib.entityPage("persons","perfil","id"));
+createMultiLangRoutes('/instituciones-publicas/:id', '/institutions/:id', lib.entityPage("institutions","perfil","id"));
+createMultiLangRoutes('/empresas/:id', '/companies/:id', lib.entityPage("companies","perfil", "id"));
+createMultiLangRoutes('/regiones/:id', '/areas/:id', lib.entityPage("areas","perfil","id"));
 
 /* GET contract pages. */
 //TODO: Agregar páginas de expediente
-router.get('/contratos/:id', lib.entityPage("contracts","contract","ocid"));
+createMultiLangRoutes('/contratos/:id', '/contracts/:id', lib.entityPage("contracts","contract","ocid"));
 
 //TODO: Ver cómo hacemos con paises y regiones
-router.get('/paises/:id/mujeresenlabolsa', lib.entityPage("countries","country-mujeres","id"));
-router.get('/paises/:id', lib.entityPage("countries","country","id"));
+createMultiLangRoutes('/paises/:id/mujeresenlabolsa', '/countries/:id/women-in-the-stock-exchange', lib.entityPage("countries","country-mujeres","id"));
+createMultiLangRoutes('/paises/:id', '/countries/:id', lib.entityPage("countries","country","id"));
 
 //STATIC PAGES
 
 /* GET about */
-router.get('/sobre-qqw', lib.staticPage("about"));
+createMultiLangRoutes("/sobre-qqw","about-qqw",lib.staticPage("about"))
+
 
 /* GET about/sources */
-router.get('/entidades-y-fuentes', lib.sourcesPage("sources"));
+createMultiLangRoutes('/entidades-y-fuentes', '/entities-and-sources', lib.sourcesPage("sources"));
 
 /* GET apis */
-router.get('/herramientas', lib.staticPage("apis"));
+createMultiLangRoutes('/herramientas', '/tools', lib.staticPage("apis"));
 
 /* GET about/investigations */
-router.get('/investigaciones', lib.staticPage("investigations"));
+createMultiLangRoutes('/investigaciones', '/research', lib.staticPage("investigations"));
 
 /* GET about/manual */
-router.get('/manual',lib.staticPage("manual"));
+createMultiLangRoutes('/manual', '/manual', lib.staticPage("manual"));
 
 /* GET about/partners */
-router.get('/aliados', lib.staticPage("partners"));
+createMultiLangRoutes('/aliados', '/allies', lib.staticPage("partners"));
 
 /* GET privacy */
-router.get('/privacidad', lib.staticPage("privacy"));
+createMultiLangRoutes('/privacidad', '/privacy', lib.staticPage("privacy"));
 
 /* GET privacy */
-router.get('/licencia', lib.staticPage("license"));
+createMultiLangRoutes('/licencia', '/license', lib.staticPage("license"));
 
 /* GET contact */
-router.get('/contacto', lib.staticPage("contact"));
+createMultiLangRoutes('/contacto', '/contact', lib.staticPage("contact"));
 
 /* POST contact */
-router.post('/send', lib.sendMailPage());
+createMultiLangRoutes('/enviar', '/send', lib.sendMailPage());
 
 /* GET mujeres español */
-router.get('/mujeres-en-la-bolsa', lib.staticPage("mujeres-en-la-bolsa",null));
+createMultiLangRoutes('/mujeres-en-la-bolsa', '/mujeres-en-la-bolsa', lib.staticPage("mujeres-en-la-bolsa",null));
 /* GET mujeres inglés */
-router.get('/women-in-the-stock-exchange', lib.staticPage("mujeres-en-la-bolsa-english",null));
+//TODO: Unify translated page
+createMultiLangRoutes('/women-in-the-stock-exchange', '/women-in-the-stock-exchange', lib.staticPage("mujeres-en-la-bolsa-english",null));
 
 /* GET gráficos mujeres iframe */
-router.get('/mujeres-en-la-bolsa/grafico1', lib.staticPage("graph1", null));
-router.get('/mujeres-en-la-bolsa/grafico2', lib.staticPage("graph2", null));
+createMultiLangRoutes('/mujeres-en-la-bolsa/grafico1','/women-in-the-stock-exchange/graph1', lib.staticPage("graph1", null));
+createMultiLangRoutes('/mujeres-en-la-bolsa/grafico2','/women-in-the-stock-exchange/graph2', lib.staticPage("graph2", null));
 
 module.exports = router;
