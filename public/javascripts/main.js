20eee34124e060f48230f954ce7e7676b52e4956
Marisol Carrillo <marisol@rindecuentas.org>
2021-07-16 18:20:56 -0500

    Fix Más filtros dropdown

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -216,16 +216,38 @@ jQuery(function() {
   });
 });
 
-$("#moreFilters").on("click", function() {
-  // console.log("moreFilters click");
-  $(".dropdown-menu-morefilters").toggleClass("d-none");
-})
+// $("#moreFilters").on("click", function() {
+//   // console.log("moreFilters click");
+//   $(".dropdown-menu-morefilters").toggleClass("d-none");
+// })
 
 // $('.dropdown-menu-morefilters').on('click', function (e) {
 //   $(this).next('ul').toggle();
 //   e.stopPropagation();
 //   e.preventDefault();
 // });
+
+$(function () {
+  // ------------------------------------------------------- //
+  // Multi Level dropdowns
+  // ------------------------------------------------------ //
+  $("ul.dropdown-menu [data-toggle='dropdown']").on("click", function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    $(this).siblings().toggleClass("show");
+
+
+    // if (!$(this).next().hasClass('show')) {
+    //   $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+    // }
+    // $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function (e) {
+    //   $('.dropdown-submenu .show').removeClass("show");
+    // });
+
+  });
+});
+
 
 $(".filter-close").on("click", function (e) {
   // console.log(e)
